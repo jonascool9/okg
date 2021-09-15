39f82f3ddacbc003a8969361eb2b3d553b2b95cd
DävScoob <82220489+jonascool9@users.noreply.github.com>
2021-09-15 13:44:49 +0200

    l

--- haus.py
+++ haus.py
@@ -1,3 +1,29 @@
-import turtle
+import turtle as t
 
-turtle.setup(width=400, height=300)
+a = 100
+b = a / 2
+
+t.setup(width=400, height=300)
+t.up()
+t.goto(-50, -50)
+t.down()
+t.left(90)
+t.forward(a)
+t.right(45)
+t.forward(b)
+t.right(90)
+t.forward(b)
+t.right(135)
+t.forward(a)
+t.left(135)
+t.forward(2 * b)
+t.right(135)
+t.forward(a)
+t.right(135)
+t.forward(2 * b)
+t.right(135)
+t.forward(a)
+if t.onkeypress(key=a):
+    t.showturtle
+elif t.onkeypress(key=d):
+    t.hideturtle
